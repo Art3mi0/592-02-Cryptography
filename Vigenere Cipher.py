dd0a58e72609b77d456695fb29df6ea1a2147413
Art3mi0 <temomez123@gmail.com>
2022-09-22 12:59:56 -0400

    Keygenerator is making key of equal filelength. Need to change. Also not encrypting character with unicode > 200

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 def keyGenerator(length):
     key = ""
     while len(key) != length:
@@ -11,15 +9,15 @@ def keyGenerator(length):
 def encrypt(key, x):
     cipher = ""
     message = x
-    min = 100
+    # min = 100
     for i in range(len(message)):
         temp = ord(message[i])+ ord(key[i])- 64
-        if min < ord(message[i]):
-            min = ord(message[i])
-            # print("Character \'" + message[i] + "\' has an ord of " + str(ord(message[i])))
         if temp > 127 & temp < 200:
             temp -= 127
-        cipher += chr(temp)
+        if temp > 200:
+            cipher += message[i]
+        else:
+            cipher += chr(temp)
     print(min)
     return cipher
 
@@ -27,12 +25,11 @@ def decrypt(key, cipher):
     message = ""
     for i in range(len(key)):
         temp = ord(cipher[i])- ord(key[i])+ 64
-        try:
-            message += chr(temp)
-        except:
-            # print("DECRYPT ERROR: " + cipher[i] + " with ord " + str(ord(cipher[i])) + " is being subtracted by " + str(temp))
-            if temp <= 0:
-                temp += 127
+        if temp <= 0:
+            temp += 127
+        if temp > 200:
+            message += cipher[i]
+        else:
             message += chr(temp)
     return message
 
@@ -50,7 +47,6 @@ def main():
 
     encryptFile = open("encrypt.html", "w")
     for j in fileEncrypt:
-        # print(j)
         try:
             encryptFile.write(j)
         except:
@@ -66,15 +62,9 @@ def main():
             line = readFile.readline()
 
     fileDecryptText = decrypt(fileKey, encryptText)
-    fileDecryptText = decrypt(fileKey, fileEncrypt)
     decryptFile = open("decrypt.html", "w")
     for j in fileDecryptText:
-        # print(j)
-        try:
-            decryptFile.write(j)
-        except:
-            print(j)
-            decryptFile.write(j.encode("utf-8"))
+        decryptFile.write(j)
     decryptFile.close()
 
 main()
