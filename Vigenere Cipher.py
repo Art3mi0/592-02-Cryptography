367a512681839b75bf1f7e4d5a228c6e8767d552
Art3mi0 <temomez123@gmail.com>
2022-09-23 22:29:13 -0400

    Assignment nearly done. Still need to make file for key and read from it.

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -1,48 +1,153 @@
 import random
 
+"""
+input   -   string
+output  -   int 1, 2, or 0
+purpose -   Method determines whether inputted character is a
+            letter, and returns a value indicating 1 for a capital,
+            2 for a lowercase, and 0 for a non-letter character     
+"""
+def letterCheck(l):
+    if (ord(l) >= 65) and (ord(l) <= 90):
+        return 1    # 1 means capital letter
+    elif (ord(l) >= 97) and (ord(l) <= 122):
+        return 2    # 2 means lowercase letter
+    else:
+        return 0    # 0 means not a letter
+
+
+"""
+input   -   int
+output  -   string
+purpose -   Method generates and returns a random string of only 
+            capital letters of inputted length    
+"""
 def keyGenerator(length):
     key = ""
     while len(key) != length:
         key += chr(random.randint(65, 90))
     return key
 
-def encrypt(key, x):
-    cipher = ""
-    message = x
-    # min = 100
-    for i in range(len(message)):
-        temp = ord(message[i])+ ord(key[i])- 64
-        if temp > 127 & temp < 200:
-            temp -= 127
-        if temp > 200:
-            cipher += message[i]
+
+"""
+input   -   string, string
+output  -   string
+purpose -   Method takes a key string and a message string and 
+            applies the Vigenere Cipher to encrypt the message.
+            The output is an encrypted version of the inputted message.
+"""
+def encrypt(key, message):
+    cipher = ""         # Initialize variables
+    fullKey = ""
+
+    while len(fullKey) != len(message):     # Algorithm for extending
+        temp = len(message) - len(fullKey)  # the key to match the
+        if temp < len(key):                 # message length
+            fullKey += key[:temp]
         else:
+            fullKey += key
+
+    for i in range(len(message)):
+        if letterCheck(message[i]) == 0:                    # 64 is subtracted because
+            cipher += message[i]                            # the key is generated with
+        elif letterCheck(message[i]) == 1:                  # only capital letters.
+            temp = ord(message[i]) + ord(fullKey[i]) - 64   # The unicode of A is 65,
+            if temp > 90:                                   # so subtracting will result
+                temp -= 26                                  # with values 1-26.
+            cipher += chr(temp)                             # 26 is subtracted when a
+        else:                                               # character passes Z or z and
+            temp = ord(message[i]) + ord(fullKey[i]) - 64   # returns back to the beginning.
+            if temp > 122:
+                temp -= 26
             cipher += chr(temp)
-    print(min)
+
     return cipher
 
+
+"""
+input   -   string, string
+output  -   string
+purpose -   Method takes a key string and a cipher string and 
+            uses the Vigenere Cipher to decrypt the message.
+            The output is a decrypted version of the inputted cipher.
+"""
 def decrypt(key, cipher):
     message = ""
-    for i in range(len(key)):
-        temp = ord(cipher[i])- ord(key[i])+ 64
-        if temp <= 0:
-            temp += 127
-        if temp > 200:
-            message += cipher[i]
+    fullKey = ""
+
+    while len(fullKey) != len(cipher):      # Algorithm for extending
+        temp = len(cipher) - len(fullKey)   # the key to match the
+        if temp < len(key):                 # message length
+            fullKey += key[:temp]
         else:
+            fullKey += key
+
+    for i in range(len(fullKey)):                       # Refer to encrypt method for
+        if letterCheck(cipher[i]) == 0:                 # explanation. Similar but reversed
+            message += cipher[i]
+        elif letterCheck(cipher[i]) == 1:
+            temp = ord(cipher[i]) - ord(fullKey[i]) + 64
+            if temp < 65:
+                temp += 26
+            message += chr(temp)
+        else:
+            temp = ord(cipher[i]) - ord(fullKey[i]) + 64
+            if temp < 97:
+                temp += 26
             message += chr(temp)
     return message
 
+
+def start(again):
+    if not again:
+        print("This program will encrypt a html file using Vigenere Cipher")
+        print("This program will generate 1 txt file and 2 html files")
+        print("In order for the program to work, ensure the html file"
+              "that is being encrypted is located in the same folder "
+              "as this program")
+
+    flag = False
+    while flag:
+        filePath = input("Enter the name of the html file (Without the \".html\" end)") + ".html"
+        try:
+            test = open(filePath, "r")
+            flag = True
+            test.close()
+        except:
+            print("ERROR: File missing or does not exist")
+
+    length = ""
+    while length.isdigit():
+        length = input("Enter desired length of key")
+        if length.isdigit():
+            if int(length) <= 1:
+                length = ""
+                print("ERROR: integer can not be 1 or less")
+        else:
+            print("ERROR: integer not entered")
+
+    generateFiles(filePath, length)
+
+
+
+def generateFiles(file, length):
+    fileText = ""
+    with open(file, "r") as testFile:
+        line = testFile.readline()
+        while line != '':
+            fileText += line
+            line = testFile.readline()
+    fileKey = keyGenerator(8)
+
 def main():
-    print(ord("€"))
-    print(chr(8364))
     fileText = ""
     with open("HTML File Extension - What is an .html file and how do I open it_.html", "r") as testFile:
         line = testFile.readline()
         while line != '':
             fileText += line
             line = testFile.readline()
-    fileKey = keyGenerator(len(fileText))
+    fileKey = keyGenerator(8)
+
     fileEncrypt = encrypt(fileKey, fileText)
 
     encryptFile = open("encrypt.html", "w")
@@ -62,9 +167,11 @@ def main():
             line = readFile.readline()
 
     fileDecryptText = decrypt(fileKey, encryptText)
+
     decryptFile = open("decrypt.html", "w")
     for j in fileDecryptText:
         decryptFile.write(j)
     decryptFile.close()
+
 
 main()
